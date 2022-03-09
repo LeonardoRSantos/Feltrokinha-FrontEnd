a60a63498e10e2a39dae198ae90044212faa3e75
Leonardo <leonardoworkbench@gmail.com>
2022-03-09 15:07:12 -0300

    Projeto Feltrokinha-FrontEnd

---
+++ src/data/Products.js
@@ -0,0 +1,16 @@
+const products = [
+    {
+        _id:"1",
+        name: "Turma Do Pocoyo",
+        image: "https://i.ibb.co/X8KVQrX/turmapocoyo.png",
+        description:
+        "Bonecos de Feltro Turma do Pocoyo",
+        price: 300,
+        countInStock: 1,
+        rating: 4,
+        numReviews: 4,
+
+    },
+];
+
+export default products;
